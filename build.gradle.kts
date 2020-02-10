8f861645f6986b141241d1300f0b793039bfa916
Nicofisi <m.lomiak@gmail.com>
2020-02-10 18:14:13 +0100

    Configure Gradle task for publishing to GitHub Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,16 @@ tasks {
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "ShulkerFramework GitHub Packages"
+            url = uri("https://maven.pkg.github.com/nicofisi/shulkerframework")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
